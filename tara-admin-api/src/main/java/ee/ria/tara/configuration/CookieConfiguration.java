b491f3009b67787d1123b54630ae48df40bac69e
Jorgen Heinsoo <jorgen.heinsoo@nortal.com>
2022-04-13 16:17:38 +0300

    AUT-635 Get session cookie name from application properties

--- tara-admin-api/src/main/java/ee/ria/tara/configuration/CookieConfiguration.java
+++ tara-admin-api/src/main/java/ee/ria/tara/configuration/CookieConfiguration.java
@@ -1,6 +1,9 @@
 package ee.ria.tara.configuration;
 
-import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +13,25 @@ public class CookieConfiguration {
     public static final String COOKIE_NAME_XSRF_TOKEN = "__Host-XSRF-TOKEN";
     public static final String COOKIE_NAME_SESSION = "__Host-SESSION";
 
+    /*
+     This solution and 'server.servlet.session.cookie.name' do not work with standalone Tomcat application.
+     If running this application in standalone Tomcat change context.xml:
+        <Context sessionCookieName="__Host-SESSION">
+        ...
+        </Context>
+    */
     @Bean
-    CookieSameSiteSupplier csrfCookieSameSiteSupplier() {
-        return CookieSameSiteSupplier.ofStrict().whenHasName(COOKIE_NAME_XSRF_TOKEN);
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> servletContext.getSessionCookieConfig().setName(COOKIE_NAME_SESSION);
     }
 
+    //Alternative for CookieSameSiteSupplier which does not work in standalone Tomcat application.
     @Bean
-    CookieSameSiteSupplier sessionCookieSameSiteSupplier() {
-        return CookieSameSiteSupplier.ofStrict().whenHasName(COOKIE_NAME_SESSION);
+    public TomcatContextCustomizer cookiesSameSiteConfiguration() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.STRICT.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
     }
 }
