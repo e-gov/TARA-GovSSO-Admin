ddbd0fe2cde9ec0cf034b9abc0543f5654b92df5
Jorgen Heinsoo <jorgen.heinsoo@nortal.com>
2022-04-20 11:29:48 +0300

    AUT-640 Cookies configuration to META-INF/context.xml in case of standalone Tomcat

--- tara-admin-api/src/main/java/ee/ria/tara/configuration/CookieConfiguration.java
+++ tara-admin-api/src/main/java/ee/ria/tara/configuration/CookieConfiguration.java
@@ -1,37 +1,32 @@
 package ee.ria.tara.configuration;
 
-import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
-import org.apache.tomcat.util.http.SameSiteCookies;
-import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ NB! Following cookie settings do not work with standalone Tomcat and are configured
+ in context.xml instead (look at webapp/META-INF/context.xml).
+*/
 @Configuration(proxyBeanMethods = false)
 public class CookieConfiguration {
 
     public static final String COOKIE_NAME_XSRF_TOKEN = "__Host-XSRF-TOKEN";
     public static final String COOKIE_NAME_SESSION = "__Host-SESSION";
 
-    /*
-     This solution and 'server.servlet.session.cookie.name' do not work with standalone Tomcat application.
-     If running this application in standalone Tomcat change context.xml:
-        <Context sessionCookieName="__Host-SESSION">
-        ...
-        </Context>
-    */
+    @Bean
+    CookieSameSiteSupplier sessionCookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofStrict().whenHasName(COOKIE_NAME_SESSION);
+    }
+
+    @Bean
+    CookieSameSiteSupplier csrfCookieSameSiteSupplier() {
+        return CookieSameSiteSupplier.ofStrict().whenHasName(COOKIE_NAME_XSRF_TOKEN);
+    }
+
     @Bean
     public ServletContextInitializer servletContextInitializer() {
         return servletContext -> servletContext.getSessionCookieConfig().setName(COOKIE_NAME_SESSION);
-    }
-
-    //Alternative for CookieSameSiteSupplier which does not work in standalone Tomcat application.
-    @Bean
-    public TomcatContextCustomizer cookiesSameSiteConfiguration() {
-        return context -> {
-            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
-            cookieProcessor.setSameSiteCookies(SameSiteCookies.STRICT.getValue());
-            context.setCookieProcessor(cookieProcessor);
-        };
     }
 }
