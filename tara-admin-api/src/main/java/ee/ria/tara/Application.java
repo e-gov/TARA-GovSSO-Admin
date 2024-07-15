612635cccd5ebdf04d44e0d684f23c80100f7057
Emma Kuppart <emma.kuppart@nortal.com>
2024-07-15 13:04:34 +0300

    AUT-1639 Fix embedded Tomcat deployment

--- tara-admin-api/src/main/java/ee/ria/tara/Application.java
+++ tara-admin-api/src/main/java/ee/ria/tara/Application.java
@@ -2,26 +2,17 @@ package ee.ria.tara;
 
 
 import ee.ria.tara.configuration.HomeController;
-import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = HomeController.class)})
-public class Application extends SpringBootServletInitializer {
+@ComponentScan(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = HomeController.class)})
+public class Application {
 
     public static void main(String[] args) {
-        configureApplication(new SpringApplicationBuilder())
-                .run(args);
-    }
-
-    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class)
-                .bannerMode(Banner.Mode.OFF);
+        SpringApplication.run(Application.class, args);
     }
 
 }
